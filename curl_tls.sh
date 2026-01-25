3b6e9c2a5e10bfb0ed944074a61cfb99ec690b9e
Igor Colombi <colombi@mailup.com>
2026-01-25 12:52:35 +0100

    20260125-12:52:35

---
+++ curl_tls.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+while true
+do
+    while IFS= read -r line; do
+        if [[ $line =~ ^#.* ]]; then
+            continue
+        fi
+        curl -s https://lab.icolombi.net/multicheck/domain/$line | jq
+    done < domains.txt
+
+    while IFS= read -r line; do
+        if [[ $line =~ ^#.* ]] || [[ -z $line ]]; then
+            continue
+        fi
+        curl -s https://lab.icolombi.net/multicheck/ip/$line | jq
+    done < ips.txt
+done
